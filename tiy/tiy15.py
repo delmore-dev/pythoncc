1495cd343d4803745e04656ab43561151c781940
Dre Elmore <delmore2341@gmail.com>
2025-02-05 15:08:56 -0500

    tiy 15.

--- tiy/tiy15.py
+++ tiy/tiy15.py
@@ -1,3 +1,6 @@
 #write a function called make_shirt() that accepts a size and the text of a message that's printed on the shirt
 def make_shirt(size,text):
-    
+    """A function to tell shirt sizes and message to be printed on the shirt"""
+    print (f"The shirt size is {size}, and you want the shirt to say {text}.")
+
+make_shirt('small', '"I <3 NY"')
